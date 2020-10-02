ee050e537a371c9af9ccfca2d1362e1ffb278236
Dominique Brisco <dbrisco93@gmail.com>
2020-10-02 18:15:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,8 +7,9 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => {
-          "Alan Anderson" => {
+      :players => [
+        {
+          :player_name => "Alan Anderson", 
           :number => "0",
           :shoe => "16",
           :points => "22",
@@ -18,7 +19,8 @@ def game_hash
           :blocks => "1",
           :slam_dunks => "1"
         },
-          "Reggie Evans" => {
+        {
+          :player_name => "Reggie Evans",
           :number => "30",
           :shoe => "14",
           :points => "12",
@@ -28,7 +30,8 @@ def game_hash
           :blocks => "12",
           :slam_dunks => "7"
         },
-          "Brook Lopez" => {
+        {
+          :player_name => "Brook Lopez",
           :number => "11",
           :shoe => "17",
           :points => "17",
@@ -38,7 +41,8 @@ def game_hash
           :blocks => "1",
           :slam_dunks => "15"
         },
-          "Mason Plumlee" => {
+        {
+          :player_name => "Mason Plumlee",
           :number => "1",
           :shoe => "19",
           :points => "26",
@@ -48,7 +52,8 @@ def game_hash
           :blocks => "8",
           :slam_dunks => "5"
         },
-          "Jason Terry" => {
+        {
+          :player_name => "Jason Terry",
           :number => "31",
           :shoe => "15",
           :points => "19",
@@ -58,7 +63,7 @@ def game_hash
           :blocks => "11",
           :slam_dunks => "1"
         }
-      }
+      ]
     },
     :away => {
       :team_name => "Charlotte Hornets",
